6be49464de958d50019930ccc71edb3b4d395003
christian burns <christianburns03@gmail.com>
2023-10-26 10:05:29 -0300

    commit II

---
+++ inicio/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class CrearPaletaFormulario(forms.Form):
+    marca = forms.CharField(max_length=30)
+    descripcion = forms.CharField(max_length=250)
+    anio = forms.IntegerField()
+    
+class BusquedaPaletaFormulario(forms.Form):
+    marca = forms.CharField(max_length=30, required=False)
--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Paleta(models.Model):
+    marca = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    anio = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.id} - {self.marca} - {self.anio}'
